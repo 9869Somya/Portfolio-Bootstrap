f7999ab6560b98a1341234b800b3e5d993559a8f
Somya Parida <paridasomya2468@gmail.com>
2024-06-17 12:34:30 +0530

    email

--- js/js-master.js
+++ js/js-master.js
@@ -30,3 +30,96 @@ function toggleTheme() {
     }
   }
 }
+
+function validate(e) {
+  let name = document.querySelector("#name").value;
+  let mobile = document.querySelector("#mobile").value;
+  let email = document.querySelector("#email").value;
+  let message = document.querySelector("#message").value;
+
+  let error = false;
+  if (name === "" || name === null) {
+    document.querySelector("#nameError").innerHTML = "Please enter your name";
+    document.querySelector("#name").style.border = "1px solid red";
+    error = true;
+  } else {
+    document.querySelector("#nameError").innerHTML = "";
+    document.querySelector("#name").style.border = "1px solid black";
+  }
+
+  if (mobile === "" || mobile === null) {
+    document.querySelector("#mobileError").innerHTML =
+      "Please enter your mobile number";
+    document.querySelector("#mobile").style.border = "1px solid red";
+    error = true;
+  } else if (mobile.length != 10 || isNaN(mobile)) {
+    document.querySelector("#mobileError").innerHTML =
+      "Please enter your valid mobile number";
+    error = true;
+  } else {
+    document.querySelector("#mobileError").innerHTML = "";
+    document.querySelector("#mobile").style.border = "1px solid black";
+  }
+
+  let emailPattern = /^[a-z]+[a-z0-9\._]{3,}@[a-z]{3,10}\.[a-z\.]{2,8}$/;
+  if (email === "" || email === null) {
+    document.querySelector("#emailError").innerHTML = "Please enter your email";
+    document.querySelector("#email").style.border = "1px solid red";
+    error = true;
+  } else if (!email.match(emailPattern)) {
+    document.querySelector("#emailError").innerHTML =
+      "Please enter your valid email";
+    error = true;
+  } else {
+    document.querySelector("#emailError").innerHTML = "";
+    document.querySelector("#email").style.border = "1px solid black";
+  }
+
+  if (message === "" || message === "Enter address" || message === null) {
+    document.querySelector("#messageError").innerHTML = "Please enter message";
+    document.querySelector("#message").style.border = "1px solid red";
+    error = true;
+  } else {
+    document.querySelector("#messageError").innerHTML = "";
+    document.querySelector("#message").style.border = "1px solid black";
+  }
+
+  if (error) {
+    e.preventDefault(); //preventDefault behaviuor of a event
+  }
+  //  else {
+  //   var params = {
+  //     from_name: document.getElementById("name").value,
+  //     mobile_no: document.getElementById("mobile").value,
+  //     email_id: document.getElementById("email").value,
+  //     message: document.getElementById("message").value,
+  //   };
+
+  //   emailjs
+  //     .send("service_f5tht34", "template_8ry6bx8", params)
+  //     .then(function (res) {
+  //       console.log("Success! " + res.status);
+  //     })
+  //     .catch(function (err) {
+  //       console.log("Failed to send message. Please try again.");
+  //     });
+  // }
+}
+
+function sendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    mobile_no: document.getElementById("mobile").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_f5tht34", "template_8ry6bx8", params)
+    .then(function (res) {
+      console.log("Success! " + res.status);
+    })
+    .catch(function (err) {
+      console.log("Failed to send message. Please try again.");
+    });
+}
